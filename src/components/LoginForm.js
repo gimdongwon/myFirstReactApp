e1280f03172755390d4b8fdbe84570750cfa3b3b
kimdongwon <ehddnjs8989@gmail.com>
2018-06-18 13:59:23 +0900

    ref, form 을 이용한 로그인

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,48 +1,51 @@
 import React from "react";
 
 export default class LoginForm extends React.Component {
-  state = {
-    username: "",
-    password: ""
-  };
+  // state = {
+  //   username: "",
+  //   password: ""
+  // };
 
-  handleUsernameChange = e => {
-    this.setState({
-      username: e.target.value
-    });
-  };
+  // handleUsernameChange = e => {
+  //   this.setState({
+  //     username: e.target.value
+  //   });
+  // };
 
-  handlePasswordChange = e => {
-    this.setState({
-      password: e.target.value
-    });
-  };
+  // handlePasswordChange = e => {
+  //   this.setState({
+  //     password: e.target.value
+  //   });
+  // };
+  // 제어되지 않는 컴포넌트 이용을 위한 주석
+
+  // ref
+
   handleLoginClick = async e => {
     // 로그인 요청
     const { onLogin } = this.props;
-    onLogin(this.state.username, this.state.password);
+    e.preventDefault();
+    onLogin(e.target.elements.username.value, e.target.elements.password.value);
   };
 
   render() {
-    const { username, password } = this.state;
+    // const { username, password } = this.state;
     return (
-      <div>
-        <label>
-          id<input
-            type="text"
-            value={username}
-            onChange={this.handleUsernameChange}
-          />
-        </label>
-        <label>
-          password<input
-            type="password"
-            value={password}
-            onChange={this.handlePasswordChange}
-          />
-        </label>
-        <button onClick={this.handleLoginClick}>로그인</button>
-      </div>
+      <form onSubmit={this.handleLoginClick}>
+        <div>
+          <label>
+            id<input type="text" name="username" ref={this.usernaemeRef} />
+          </label>
+          <label>
+            password<input
+              type="password"
+              name="password"
+              ref={this.passwordRef}
+            />
+          </label>
+          <button>로그인</button>
+        </div>
+      </form>
     );
   }
 }
--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -6,14 +6,22 @@ const { Provider, Consumer } = React.createContext();
 class UserProvider extends React.Component {
   login = async (username, password) => {
     // 로그인 요청
-    const res = await todoAPI.post("/users/login", {
-      username: username,
-      password: password
-    });
-    //localStorage에 토큰 저장
-    localStorage.setItem("token", res.data.token);
-    // page 전환
-    this.props.onLogin();
+    try {
+      const res = await todoAPI.post("/users/login", {
+        username: username,
+        password: password
+      });
+      //localStorage에 토큰 저장
+      localStorage.setItem("token", res.data.token);
+      // page 전환
+      this.props.onLogin();
+    } catch (e) {
+      if (e.response) {
+        if (e.response && e.response.status === 400) {
+          alert("id혹은 password가 일치 하지 않습니다");
+        }
+      }
+    }
   };
   render() {
     const value = {
